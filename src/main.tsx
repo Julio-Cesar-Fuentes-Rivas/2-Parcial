5f50267f23eac9e4ba65e43bfd409a7c09ea7605
Julio-Cesar-Fuentes-Rivas <juliocesarfuentesrivas06@gmail.com>
2026-05-05 10:34:03 -0400

    actualizacion manual de componentes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './App.tsx'
+//actualizacion de la ruta del componente NotesList
 import { NotesList } from './components/templates/NotesList/NotesList';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
