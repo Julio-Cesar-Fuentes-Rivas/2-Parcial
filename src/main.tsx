8cbc75e3b7cd86db203dc3583e405cbd9e9e796d
Julio-Cesar-Fuentes-Rivas <juliocesarfuentesrivas06@gmail.com>
2026-05-05 09:06:04 -0400

    actualizacion

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-
+//import App from './App.tsx'
+import { NotesList } from './components/templates/NotesList/NotesList';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <NotesList />
   </StrictMode>,
 )
